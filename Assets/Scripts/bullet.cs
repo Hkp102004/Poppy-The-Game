b6fafa5845391ee97c14b947d3dbe8d7e219bebd
HKP102004 <harsh15062004@gmail.com>
2026-02-03 21:23:57 +0530

    feat: make the bullet logic

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 10;
     void Start()
     {
         
@@ -11,6 +12,6 @@ public class bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
 }
