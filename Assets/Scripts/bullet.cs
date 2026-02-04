782e6f26e65308c44135b120ee61eac9d82213c0
HKP102004 <harsh15062004@gmail.com>
2026-02-04 14:34:09 +0530

    refacter: clean the code

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private float speed = 10;
-    [SerializeField] private float deathzone = 18;
-    [SerializeField] private Transform player;
+    [SerializeField] private float speed = 15f; //the speed of the bullet
+    [SerializeField] private float deathzone = 18; //the distance after which the bullet should be destroyed
+    [SerializeField] private Transform player; //refernce of player position
     void Start()
     {
         
@@ -14,7 +14,7 @@ public class bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        transform.Translate(Vector3.down * speed * Time.deltaTime); //movement of bullet
 
         if(transform.position.x > player.position.x + deathzone) //to destroy the game object
         {
