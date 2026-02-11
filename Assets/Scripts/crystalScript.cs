2eadae771a0e5edaad27963efc9fde73b319c107
HKP102004 <harsh15062004@gmail.com>
2026-02-11 15:32:27 +0530

    feat: make crystal script and UIManager script and connect them

---
+++ Assets/Scripts/crystalScript.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class crystalScript : MonoBehaviour
+{
+    [TextArea]
+    public String message;
+    public UIManager uIManager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+}
