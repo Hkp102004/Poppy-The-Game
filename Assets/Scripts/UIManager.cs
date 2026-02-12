2639e9ba68604a7ced3341c8f5758c6adcab7de8
HKP102004 <harsh15062004@gmail.com>
2026-02-12 15:53:24 +0530

    feat: make the score or memory crytal tracker score in UI and logic in uiManager script

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,8 @@ public class UIManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Text messageText;
     [SerializeField] private float waitTime;
+    [SerializeField] private Text score;
+    private int scorevar=0;
     void Start()
     {
         messageText.gameObject.SetActive(false);
@@ -16,18 +18,24 @@ public class UIManager : MonoBehaviour
             Debug.LogError("message text is missing from uimanager script");
             return;
         }
+        if(score==null)
+        {
+            Debug.LogError("The score text or memory crystal text is missing in uiscript");
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        score.text = scorevar.ToString();
     }
 
     public void ShowMessage(string msg)
     {
         messageText.text = msg;
         messageText.gameObject.SetActive(true);
+        scorevar+=1;
         StartCoroutine(MessageCooldown());
     }
 
