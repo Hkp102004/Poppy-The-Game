1f37e463b77bbb1d1e8d63c7b5915d9724937286
HKP102004 <harsh15062004@gmail.com>
2026-02-12 21:20:49 +0530

    feat: imporve camera follow loigc so game feels smooth

--- Assets/Scripts/cameraScript.cs
+++ Assets/Scripts/cameraScript.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,6 +7,7 @@ public class cameraScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Transform player;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothSpeed;
 
     void Start()
     {
@@ -19,12 +21,17 @@ public class cameraScript : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x + offset.x ,player.position.y +offset.y,offset.z);
+        // transform.position = new Vector3(player.position.x + offset.x ,player.position.y +offset.y,offset.z);
 
-        if(player == null)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        }
+        // if(player == null)
+        // {
+        //     transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        // }
+        if (player == null) return;
+
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, offset.z);
     }
 }
     
