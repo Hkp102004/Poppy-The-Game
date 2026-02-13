67d4b9247a951bb7997c12333b99afd2a8e24560
HKP102004 <harsh15062004@gmail.com>
2026-02-13 20:32:55 +0530

    feat: make thorn sound and play it when hit

--- Assets/Scripts/thorns.cs
+++ Assets/Scripts/thorns.cs
@@ -7,9 +7,10 @@ public class thorns : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private GameObject player;
     [SerializeField] private float distance = 10;
+    private AudioSource thornSound;
     void Start()
     {
-
+        thornSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +23,7 @@ public class thorns : MonoBehaviour
     {
         if(collider.gameObject.tag == "Player")
         {
+            thornSound.Play();
             player.transform.position = new  Vector3(player.transform.position.x - distance, player.transform.position.y + 3, player.transform.position.z);
         }
     }
