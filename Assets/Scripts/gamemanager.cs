e69ed4a90f3ac3be70b15035d08fcf8ecc503bbd
Hekey <87144005+Hkp102004@users.noreply.github.com>
2026-03-31 23:02:00 +0530

    feat: made the logic to connect to start the pause menu on esc keydown

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -1,8 +1,26 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-
 public class gamemanager : MonoBehaviour
 {
+    UIManager ui; //ui manager script reference
+    bool paused;
+
+    void Start()
+    {
+        ui = GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>();
+        if(ui == null)
+        {
+            Debug.LogError("the uimanager script is missing from gamemanager script");
+            return;
+        }
+    }
+
+    void Update()
+    {
+        Pause();
+    }
     public void Restart()
     {
         SceneManager.LoadScene(1); // 1 is for game scene
@@ -10,5 +28,25 @@ public class gamemanager : MonoBehaviour
     public void Main_menu()
     {
         SceneManager.LoadScene(0); //0 is for main menu scene
+    }
+    void Pause() //to check and pause the game
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = true;
+            ui.pause();
+
+            if(paused)
+            {
+                paused = false;
+                ui.resume();
+            }
+        }
+    }
+
+    void resume()
+    {
+        paused = false;
+        ui.resume();
     }
 }
