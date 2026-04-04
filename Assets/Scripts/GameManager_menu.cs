1e8180c1ffa9efec3ba93c02cc5bc85b7e4ebc94
Hekey <87144005+Hkp102004@users.noreply.github.com>
2026-04-04 22:27:10 +0530

    feat: make a quit button in menu
    made a quit button in menu to leave the game and the code is in GameManager_menu script

--- Assets/Scripts/GameManager_menu.cs
+++ Assets/Scripts/GameManager_menu.cs
@@ -7,4 +7,9 @@ public class GameManager_menu : MonoBehaviour
     {
         SceneManager.LoadScene(1); //game scene
     }
+
+    public void quit() //quits the game when player clicks on quit button in menu
+    {
+        Application.Quit();
+    }
 }
